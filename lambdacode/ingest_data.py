4ca23cdd4692db283273de984ab5bb7974f4ced5
robi <kali_robi@hotmail.com>
2024-12-16 21:26:01 +0200

    working psycopg2 layer with docker template, back to textract

---
+++ lambdacode/ingest_data.py
@@ -0,0 +1,36 @@
+import json
+import boto3
+import psycopg2
+import time
+from datetime import datetime
+
+
+s3_client = boto3.client('s3')
+
+
+textract_client = boto3.client('textract')
+
+
+rds_host = "localhost"
+db_name = "2mo"
+db_user = "admin"
+db_password = "admin123"
+
+def lambda_handler(event, context):
+
+    bucket_name = event['detail']['bucket']['name']
+    file_key = event['detail']['object']['key']
+    
+
+    s3_object = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    image_data = s3_object['Body'].read()
+
+
+    response = textract_client.start_document_text_detection(
+        DocumentLocation={'S3Object': {'Bucket': bucket_name, 'Name': file_key}}
+    )
+    
+    # Get the Job ID
+    job_id = response['JobId']
+    print(f"Started Textract job with ID: {job_id}")
+
